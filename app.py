92daef0fcaa688fd7f0555a663235da91b3ea759
Shanto Noor <shantonoor900@gmail.com>
2022-08-21 16:59:55 +0600

    Update

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ TOKEN = '2560f5ab98edbed5a67062108c50bb33'
 HELLO_PACKET = '21310020ffffffffffffffffffffffffffffffffffffffffffffffffffffffff'
 GET_INFO_DATA = '5edc00f3fd2d5d3bec1c5ff56e78e2645fc529220e5f09e59f4d9bb58762ef5f9620fa9d2215ec0621b71075d2cd7f2d'
 GET_POWER_STATUS_DATA = 'afe0e9c5e26a2a26bbba23bd452e487e47e227086b5d55aac62949c533e0ea44782fde3e3f1d235eff60e25b31c8fa77'
+POWER_OFF_DATA = '6aaebcddf12b4ee2d7319208d9031fefb653297a3d2bfd56efffb0659c6b6b760e8560ffd2afd041e13ae8e8c0ad8b42'
+POWER_ON_DATA = '6aaebcddf12b4ee2d7319208d9031fefb653297a3d2bfd56efffb0659c6b6b76966833b94b9e6e8059f0de76850d358e'
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
@@ -23,7 +25,8 @@ data = data.hex()
 print(data)
 
 # packet_payload = hex_to_byte(GET_INFO_DATA)
-packet_payload = hex_to_byte(GET_POWER_STATUS_DATA)
+# packet_payload = hex_to_byte(GET_POWER_STATUS_DATA)
+packet_payload = hex_to_byte(POWER_OFF_DATA)
 packet_length = hex(len(packet_payload) + 32)[2:].rjust(4, '0')
 packet_head = hex_to_byte( data[:4] + packet_length + data[8:32] + TOKEN )
 
--- mi.js
+++ mi.js
@@ -1,8 +1,10 @@
 const miio = require('miio');
 
-miio.device({ address: '192.168.0.50', token: '2560f5ab98edbed5a67062108c50bb33' })
-  .then(device => {
+miio.device({ address: '192.168.0.51', token: '2560f5ab98edbed5a67062108c50bb33' })
+  .then(async function (device) {
     console.log('Connected to', device);
     device.power().then(isOn => console.log('Outlet power:', isOn));
+    // await device.setPower(true);
+    // wait device.setPower(false);
     device.destroy();
   }).catch(err => console.error(err));
