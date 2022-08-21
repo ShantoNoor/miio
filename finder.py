f95ecba0b35c71ab7836c607ae2e8d277dac8628
Shanto Noor <shantonoor900@gmail.com>
2022-08-21 11:48:25 +0600

    Update

---
+++ finder.py
@@ -0,0 +1,9 @@
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+sock.bind(("0.0.0.0", 12345))
+
+sock.sendto(bytes.fromhex('21310020ffffffffffffffffffffffffffffffffffffffffffffffffffffffff'), ('255.255.255.255', 54321))
+data, addr = sock.recvfrom(1024)
+print(data.hex())
