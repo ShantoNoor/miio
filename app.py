0fab281259e838716d895c03696d874bbaf18b3a
Shanto Noor <shantonoor900@gmail.com>
2022-12-24 15:00:58 +0600

    Update

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import sys
 import socket
 import hashlib
+from miio_data import *
 
 print(sys.argv)
 
@@ -10,12 +11,7 @@ def hex_to_byte(hex: str) -> bytes:
 def md5(data: bytes) -> bytes:
     return hashlib.md5(data).digest()
 
-TOKEN = '2560f5ab98edbed5a67062108c50bb33'
-HELLO_PACKET = '21310020ffffffffffffffffffffffffffffffffffffffffffffffffffffffff'
-GET_INFO_DATA = '5edc00f3fd2d5d3bec1c5ff56e78e2645fc529220e5f09e59f4d9bb58762ef5f9620fa9d2215ec0621b71075d2cd7f2d'
-GET_POWER_STATUS_DATA = 'afe0e9c5e26a2a26bbba23bd452e487e47e227086b5d55aac62949c533e0ea44782fde3e3f1d235eff60e25b31c8fa77'
-POWER_OFF_DATA = '6aaebcddf12b4ee2d7319208d9031fefb653297a3d2bfd56efffb0659c6b6b760e8560ffd2afd041e13ae8e8c0ad8b42'
-POWER_ON_DATA = '6aaebcddf12b4ee2d7319208d9031fefb653297a3d2bfd56efffb0659c6b6b76966833b94b9e6e8059f0de76850d358e'
+TOKEN = '3a8ff161dd25b4cc332b8afd874b2615'
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
